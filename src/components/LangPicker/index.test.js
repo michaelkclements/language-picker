50f009d5b700448b3cf9b91d97cecb134f2a11fe
Michael K. Clements <michaelkc@me.com>
2017-07-03 18:44:36 +0100

    Added initial test files to Header and LangPicker

--- src/components/LangPicker/index.test.js
+++ src/components/LangPicker/index.test.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Header from '../Header'
+import LangPicker from '../LangPicker'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<Header />, div)
+  ReactDOM.render(<LangPicker />, div)
 })
